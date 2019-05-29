1d82e108e840d679b03a4d6c4df66d879b591bab
Austin Mleziva <AustinMleziva@gmail.com>
2019-05-29 13:44:24 -0500

    adding eslint updating getrecipe method

--- server/library/recipeParser.js
+++ server/library/recipeParser.js
@@ -1,101 +1,126 @@
 const request = require('request');
-const HTMLParser = require("node-html-parser");
+const HTMLParser = require('node-html-parser');
 
-var recipeParser = (function () {
-    'use strict';
+const recipeParser = (function recipeParser() {
+  let hostName; let fullSiteContents; let htmlRoot;
 
-    var hostName = '';
-
-    function getRecipe(url, callback) {
-        hostName = new URL(url).hostname;
-        var printUrl = _createPrintUrl(url);
-        return _getPrintableText(printUrl, callback);
-    };
-    function _getPrintableText(url, callback) {
-        request(url, { json: false }, (err, res, body) => {
-            if (err) { return console.log(err); }
-            var recipe = _getPrintableRecipe(body, '.recipe-print__container');
-            return callback(recipe, false);
-        });
-    };
-    function _createPrintUrl(url) {
-        var printUrl = '';
-        if (hostName.includes('allrecipes.com')) {
-            var lastSlash = _nthIndex(url, '/', 6)
-            var mainUrl = url.substr(0, lastSlash)
-            printUrl = mainUrl + '/print';
+  function _doRequest(url) {
+    return new Promise(((resolve, reject) => {
+      request(url, (error, res, body) => {
+        if (!error && res.statusCode === 200) {
+          resolve(body);
+        } else {
+          reject(error);
         }
-        //allrecieps pattern: https://www.allrecipes.com/recipe/245559/fall-infused-mashed-potatoes/print/
-        //hostname/recipe/{id}/{name}/print
-        return printUrl;
-    };
-    function _getPrintableRecipe(content, selector) {
-        var root = HTMLParser.parse(content);
-        var recipe = root.querySelector(selector);
-        return recipe.toString();
-    };
-    function _nthIndex(str, pat, n) {
-        var L = str.length, i = -1;
-        while (n-- && i++ < L) {
-            i = str.indexOf(pat, i);
-            if (i < 0) break;
-        }
-        return i;
-    };
-    async function executeAsyncTask() {
-        const valueA = await functionA()
-        const valueB = await functionB(valueA)
-        return function3(valueA, valueB)
+      });
+    }));
+  }
+  function _nthIndex(str, pat, numToGet) {
+    const L = str.length; let
+      i = -1; let n = numToGet;
+    while (n-- && i++ < L) {
+      i = str.indexOf(pat, i);
+      if (i < 0) break;
     }
-    async function getRecipe2(url, callback) {
-        //get page as string
-        //search with regex for link to print page
-        //strip out querystrings
-        //request that print page
-        //get title
-        //get title container and return just the contents of that
-        let fullSiteContents, printSiteContents, printUrl;
-        try {
-            fullSiteContents = await _doRequest(url)
-        } catch (err) {
-            logger.error('Http error', err)
-            return res.status(500).send()
-        };
-        printUrl = _findPrintUrl(fullSiteContents);
-        try {
-            fullSiteContents = await _doRequest(printUrl)
-        } catch (err) {
-            logger.error('Http error', err)
-            return res.status(500).send()
-        };
-        return fullSiteContents;
-    };
+    return i;
+  }
+  function _createPrintUrl(url) {
+    let printUrl = '';
+    if (hostName.includes('allrecipes.com')) {
+      const lastSlash = _nthIndex(url, '/', 6);
+      const mainUrl = url.substr(0, lastSlash);
+      printUrl = `${mainUrl}/print`;
+    }
+    // allrecieps pattern: https://www.allrecipes.com/recipe/245559/fall-infused-mashed-potatoes/print/
+    // hostname/recipe/{id}/{name}/print
+    return printUrl;
+  }
+  function _getRecipeName(selector) {
+    const nameNode = htmlRoot.querySelector(selector);
+    return nameNode.text;
+  }
+  function _getRecipeBody(selector) {
+    const recipe = htmlRoot.querySelector(selector);
+    return recipe.toString();
+  }
+  function _parseHtml(content) {
+    htmlRoot = HTMLParser.parse(content, {
+      script: false,
+      style: false,
+      pre: false,
+    });
+  }
 
-    function _findPrintUrl(str) {
-        const regex = /(="(https?|www|\/\/www)(.+?)print(.+?)")/gm;
-        var dirtyUrl = regex.exec(str)[0];
-        //remove query strings
-        const queryStringRegex = /(="(.+?)(\?|"))/gm;
-        var cleanerUrl = queryStringRegex.exec(dirtyUrl)[0];
-        //trim beginning and ending characters
-        var cleanUrl = cleanerUrl.substr(2).slice(0, -1);
-        return cleanUrl;
-    };
-    function _doRequest(url) {
-        return new Promise(function (resolve, reject) {
-          request(url, function (error, res, body) {
-            if (!error && res.statusCode == 200) {
-              resolve(body);
-            } else {
-              reject(error);
-            }
-          });
-        });
-      }
-    return {
-        getRecipe: getRecipe,
-        getRecipe2: getRecipe2
-    };
+
+  async function tryGetIndexedRecipe(url) {
+    hostName = new URL(url).hostname;
+    // load hostname from indexfile
+    const printUrl = _createPrintUrl(url);
+    try {
+      fullSiteContents = await _doRequest(printUrl);
+    } catch (err) {
+      // return error here or something
+    }
+    _parseHtml(fullSiteContents);
+    const recipeContents = _getRecipeBody('.recipe-print__container');
+    const recipeName = _getRecipeName('.recipe-print__title');
+    return { url, recipeContents, recipeName };
+  }
+
+  async function getRecipe(url) {
+    let recipeObj;
+    // get known recipe
+    recipeObj = tryGetIndexedRecipe(url);
+    if (recipeObj === null) {
+      // index doesn't have that site;
+      // get unknown recipe
+      recipeObj = 'test';
+    }
+    return recipeObj;
+  }
+
+
+  async function getRecipe2(url, callback) {
+    // if page is not stored in list
+    // get page as string
+    // search with regex for link to print page
+    // strip out querystrings
+    // request that print page
+    // get title
+    // print page and set title
+    let fullSiteContents; let printSiteContents; let
+      printUrl;
+    try {
+      fullSiteContents = await _doRequest(url);
+    } catch (err) {
+      logger.error('Http error', err);
+      return res.status(500).send();
+    }
+    printUrl = _findPrintUrl(fullSiteContents);
+    try {
+      fullSiteContents = await _doRequest(printUrl);
+    } catch (err) {
+      logger.error('Http error', err);
+      return res.status(500).send();
+    }
+    return fullSiteContents;
+  }
+
+  function _findPrintUrl(str) {
+    const regex = /(="(https?|www|\/\/www)(.+?)print(.+?)")/gm;
+    const dirtyUrl = regex.exec(str)[0];
+    // remove query strings
+    const queryStringRegex = /(="(.+?)(\?|"))/gm;
+    const cleanerUrl = queryStringRegex.exec(dirtyUrl)[0];
+    // trim beginning and ending characters
+    const cleanUrl = cleanerUrl.substr(2).slice(0, -1);
+    return cleanUrl;
+  }
+
+  return {
+    getRecipe,
+    getRecipe2,
+  };
 }());
 
 module.exports = recipeParser;
--- server/recipeRoutes.js
+++ server/recipeRoutes.js
@@ -1,45 +1,44 @@
-var express = require('express');
-var router = express.Router();
-var socketSingleton = require('./library/socketSingleton');
+const express = require('express');
+const socketSingleton = require('./library/socketSingleton');
+const RecipeParser = require('./library/recipeParser');
+const recipeCache = require('./library/recentCache');
 
-const RecipeParser = require("./library/recipeParser");
-const recipeCache = require("./library/recentCache");
-var i = 0;
+const router = express.Router();
 /* GET home page. */
-router.get('/', function (req, res, next) {
-    res.json('Express RESTful API Time:' + new Date(Date.now()).toTimeString());
+router.get('/', (req, res) => {
+  res.json(`Express RESTful API Time:${new Date(Date.now()).toTimeString()}`);
 });
 
-router.get('/recipe', function (req, res, next) {
-    try{
-        var thisRequest = i++;
-        var searchedUrl = req.query.recipeUrl || "https://www.allrecipes.com/recipe/245559/fall-infused-mashed-potatoes/?internalSource=staff%20pick&referringId=1540&referringContentType=Recipe%20Hub";
-        //try to get name from url
-        //save search to cache
-        recipeCache.addSearch({name: "t " +thisRequest, url: searchedUrl})
-        socketSingleton.io.emit('newSearch', {name: "t " + thisRequest, url: searchedUrl});
-    RecipeParser.getRecipe(searchedUrl, function (err, data) {
-        if (err) return res.json(err);
-        res.json(data);
-    });
-}
-catch(err){
+router.get('/recipe', async (req, res) => {
+  try {
+    const searchedUrl = req.query.recipeUrl || 'https://www.allrecipes.com/recipe/245559/fall-infused-mashed-potatoes/?internalSource=staff%20pick&referringId=1540&referringContentType=Recipe%20Hub';
+    const recipeObject = await RecipeParser.getRecipe(searchedUrl);
+    const recipeContent = recipeObject.recipeContents;
+    const recipeName = recipeObject.recipeName;
+    const recipeUrl = recipeObject.url;
+    const searchHistorObj = { name: recipeName, url: recipeUrl };
+    // try to get name from url
+    // save search to cache
+    recipeCache.addSearch(searchHistorObj);
+    socketSingleton.io.emit('newSearch', searchHistorObj);
+    res.json(recipeContent);
+  } catch (err) {
     console.log(err);
     res.json(err.toString());
-}
+  }
 });
 router.get('/recipe2', async (req, res, next) => {
-    try {
-        var searchedUrl = req.query.recipeUrl || "https://www.allrecipes.com/recipe/245559/fall-infused-mashed-potatoes/?internalSource=staff%20pick&referringId=1540&referringContentType=Recipe%20Hub";
-        const data = await RecipeParser.getRecipe2(searchedUrl);
-        res.json(data);
-      } catch (e) {
-        //this will eventually be handled by your error handling middleware
-        next(e) 
-      }
+  try {
+    const searchedUrl = req.query.recipeUrl || 'https://www.allrecipes.com/recipe/245559/fall-infused-mashed-potatoes/?internalSource=staff%20pick&referringId=1540&referringContentType=Recipe%20Hub';
+    const data = await RecipeParser.getRecipe2(searchedUrl);
+    res.json(data);
+  } catch (e) {
+    // this will eventually be handled by your error handling middleware
+    next(e);
+  }
 });
-router.get('/recipe/recentSearches', function(req, res, next){
-    res.json(recipeCache.recentSearches);
+router.get('/recipe/recentSearches', (req, res, next) => {
+  res.json(recipeCache.recentSearches);
 });
 
 module.exports = router;
